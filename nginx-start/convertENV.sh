8da5a858df02f5a37271060dd3033a12b43d9ba0
James Drummond <james@devcomb.com>
2018-08-23 16:16:06 -0500

    Update convertENV.sh

--- nginx-start/convertENV.sh
+++ nginx-start/convertENV.sh
@@ -2,3 +2,4 @@
 set -e
 envsubst < ${NGINX_CONFIGURATION_PATH}/upstream.conf > ${NGINX_CONFIGURATION_PATH}/upstream.conf
 envsubst '${DEV_USERS_WITH_LOCATIONS}' < ${NGINX_CONFIGURATION_PATH}/split_traffic.conf > ${NGINX_CONFIGURATION_PATH}/split_traffic.conf
+envsubst '${DEV_EMAILS_WITH_LOCATIONS}' < ${NGINX_CONFIGURATION_PATH}/split_traffic.conf > ${NGINX_CONFIGURATION_PATH}/split_traffic.conf
