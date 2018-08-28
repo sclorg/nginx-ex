dc5e4a083877c9e3b609095e037b9850f79d2d9d
James Drummond <james@devcomb.com>
2018-08-28 14:39:36 -0500

    Update convertENV.sh

--- nginx-start/convertENV.sh
+++ nginx-start/convertENV.sh
@@ -4,5 +4,6 @@ export OAUTH_AUTH_SERVER=$(curl https://${KUBERNETES_SERVICE_HOST}/.well-known/o
 #below doesn't work because ${HOME}/.profile not loaded
 #echo "export OAUTH_AUTH_SERVER=${OAUTH_AUTH_SERVER}" >> ${HOME}/.profile
 envsubst < ${NGINX_CONFIGURATION_PATH}/upstream.conf > ${NGINX_CONFIGURATION_PATH}/upstream.conf
+envsubst '${OAUTH_AUTH_SERVER}' < ${NGINX_DEFAULT_CONF_PATH}/user-redirect.conf > ${NGINX_DEFAULT_CONF_PATH}/user-redirect.conf
 #envsubst '${DEV_EMAILS_WITH_LOCATIONS}' < ${NGINX_CONFIGURATION_PATH}/split_traffic.conf > ${NGINX_CONFIGURATION_PATH}/split_traffic.conf
 echo "Finished convertENV.sh"
