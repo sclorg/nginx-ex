e227087f38a30cf6de153c7b171c21ccb28f4e0f
James Drummond <james@devcomb.com>
2018-08-28 14:16:33 -0500

    Update convertENV.sh

--- nginx-start/convertENV.sh
+++ nginx-start/convertENV.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 export OAUTH_AUTH_SERVER=$(curl https://${KUBERNETES_SERVICE_HOST}/.well-known/oauth-authorization-server -k|python -c "import sys, json; print(json.load(sys.stdin)['issuer'])")
+echo "export OAUTH_AUTH_SERVER=${OAUTH_AUTH_SERVER}" >> ${HOME}/.profile
 envsubst < ${NGINX_CONFIGURATION_PATH}/upstream.conf > ${NGINX_CONFIGURATION_PATH}/upstream.conf
 envsubst '${DEV_EMAILS_WITH_LOCATIONS}' < ${NGINX_CONFIGURATION_PATH}/split_traffic.conf > ${NGINX_CONFIGURATION_PATH}/split_traffic.conf
 echo "Finished convertENV.sh"
