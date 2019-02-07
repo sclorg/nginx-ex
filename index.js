1648f64eddabb7b83bfa6cd8d23f968a5f5368c5
devcomb <james@devcomb.com>
2019-02-07 02:41:23 +0000

    fixup

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const https = require('https');
 const http = require('http');
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-var sa_token = 'Bearer '+process.env["SA_TOKEN"];
-var os_console_host = process.env["OS_CONSOLE_HOST"];
-var os_console_port = process.env["OS_CONSOLE_PORT"];
-var options;
 
 //Todo ability to check that role that role has correct access to required resources
 //Key pair lookup with ei [{"pods":"get"},{"pods":"list"},{"pods":"watch"}] - Resource pods and verbs get,list,watch
@@ -26,82 +22,117 @@ var required_role_resources = process.env["REQUIRED_ROLE_RESOURCES"];
 //Test by using user/group/sa token
 //oc get groups -o json --loglevel=9 --token=<token>
 
-function setOptions(namespace,sa_token){
-    options = {
+function setOptions(headersObj){
+    return options = {
         headers: {
             'Accept': 'application/json',
-            'Authorization': sa_token
+            'Authorization': headersObj.sa_token
         },
-    hostname: os_console_host,
-    port: os_console_port,
-    path: `/apis/authorization.openshift.io/v1/namespaces/${namespace}/rolebindings/`,
-    method: 'GET'
+        timeout: 100,
+        hostname: headersObj.os_console_host,
+        port: headersObj.os_console_port,
+        path: `/apis/authorization.openshift.io/v1/namespaces/${headersObj.namespace}/rolebindings/`,
+        method: 'GET'
     };
+    
 };
 
 app.get('/', function (req, res) {
-  if (! req.accepts('json')) {
-    res.status(400).send('Header "Accept" must be set to application/json.');
-    return;
-  }
-  var subject = req.headers['x-subject'];
-  var group = req.headers['x-subject-group'];
-  var sa_token = req.headers['authorization'];
-  if(req.headers['x-subject']){
-    var namespace = req.headers['x-namespace'];
-    setOptions(namespace,sa_token);
-    https.get(options, (resInternal) => {
-        let body = '';
-        if(resInternal.statusCode==200){
-            resInternal.on('data', (chunk) => {
-                try {
-                    body += chunk;
-                } catch(err) {
-                    console.error(err)
-                }
-            });
-            resInternal.on('end', () => {
-                var authorized = false;
-                try {
-                    const data = JSON.parse(body);
-                    _.each(data.items,(value, key, list) => {
-                        //value.groupNames
-                        if(value.userNames){
-                            if(_.contains(value.userNames,subject) && (_.contains(value.groupNames,group) || ! group ) ){                                
-                                authorized = true;
+    if (! req.accepts('json')) {
+        res.status(400).send('Header "Accept" must be set to application/json.');
+        return;
+    }
+    var headers = { 
+        os_console_host: req.headers['x-oauth-host'],
+        os_console_port: req.headers['x-oauth-port'],
+        subject: req.headers['x-subject'],
+        group: req.headers['x-subject-group'],
+        sa_token: req.headers['authorization'],
+        namespace: req.headers['x-namespace']
+    };
+    // console.log("statusCode: ", req.statusCode); // <======= Here's the status code
+    // console.log("headers: ", req.headers);
+
+    if(checkHeaders(headers,res)){
+        var options = setOptions(headers);
+        https.get(options, (resInternal) => {
+            let body = '';
+            if(resInternal.statusCode==200){
+                resInternal.on('data', (chunk) => {
+                    try {
+                        body += chunk;
+                    } catch(err) {
+                        console.error(err)
+                    }
+                });
+                resInternal.on('end', () => {
+                    var authorized = false;
+                    try {
+                        const data = JSON.parse(body);
+                        _.each(data.items,(value, key, list) => {
+                            if(value.userNames){
+                                if(_.contains(value.userNames,headers.subject) && (_.contains(value.groupNames,headers.group) || ! headers.group ) ){                                
+                                    authorized = true;
+                                }
                             }
                         }
+                    )
+                    if(authorized){
+                        res.status(200).send('ok');
                     }
-                )
-                if(authorized){
-                    res.status(200).send('ok');
+                    else{
+                        res.status(401).send('User and/or Group not authorized.');
+                    }
+                    } catch (er) {
+                    //   // uh oh! bad json!
+                        console.error('er: ', er);
+                        res.statusCode = 400;
+                        res.end(`error: ${er.message}`);
+                    }
+                });
+                resInternal.on('socket', function (socket) {
+                    socket.setTimeout(100);  
+                    socket.on('timeout', function() {
+                        resInternal.abort();
+                    });
+                });                
+            }
+            else{
+                res.status(resInternal.statusCode).end(`Message: ${resInternal.statusMessage}`);
+                // console.error("statusCode: ", resInternal.statusCode); // <======= Here's the status code
+                // console.error("headers: ", resInternal.headers);
+            }
+            }).on('error', (e) => {
+                if(e.code === 'ENOTFOUND'){
+                    res.statusCode = 404;
                 }
-                else{
-                    res.status(401).send('User and/or Group not authorized.');
-                }
-                } catch (er) {
-                //   // uh oh! bad json!
-                    res.statusCode = 400;
-                    return res.end(`error: ${er.message}`);
-                    console.error('er: ', er);
-                }
+                res.end(`Error: ${e.message}`);
             });
-            
-        }
-        else{
-            res.status(resInternal.statusCode).end(`Message: ${resInternal.statusMessage}`);
-            // console.error("statusCode: ", resInternal.statusCode); // <======= Here's the status code
-            // console.error("headers: ", resInternal.headers);
-        }
-        }).on('error', (e) => {
-        console.error(e);
-        });
-  }
-  else{
-        res.status(400).end(`Message: Header variable 'X-Subject' is undefined.`);
-  }
+    }
 });
 
+
+function checkHeaders(headers,res){
+    var headersSet = true;
+    var badkeys={};
+    for(key in headers){
+        //Checks if headers are empty or contain no values
+        if((headers[key] === "" || ! headers[key] )  && ! (key === "group") ){
+            headersSet = false;
+            badkeys[key] = headers[key];
+        }
+    }
+    if(!headersSet){
+        res.statusCode = 400;
+        var keysStr='';
+        for(key in badkeys){
+            keysStr=keysStr+key+",";
+        }
+        keysStr = keysStr.slice(0, -1);
+        res.end(`Message: Header variable(s) '${keysStr}' is undefined.`);
+    }
+    return headersSet;
+}
 
 var server = app.listen(8080, function () {
   console.log('App listening on port 8080.');
--- test.js
+++ test.js
@@ -4,6 +4,8 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 var sa_token = 'Bearer '+process.env["SA_TOKEN"];
 var namespace = 'console';
 var subject ='james@devcomb.com'
+var host ='console.devcomb.com'
+var port = '8443'
 
 describe('Checking Authorization Backend Helper App:', function () {
   var server;
@@ -14,74 +16,107 @@ describe('Checking Authorization Backend Helper App:', function () {
     server.close();
   });
   it(`responds to / for user "${subject}" without group criteria`, function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','application/json')
-    .set('Authorization', sa_token)
-    .set('X-Subject', subject)
-    .set('X-Subject-Group', ``)
-    .set('X-Namespace', `${namespace}`)
-    .expect(200, done);
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', sa_token)
+        .set('X-Subject', subject)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(200, done);
   });
   it(`responds to / for user "${subject}" in group "developer"`, function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','application/json')
-    .set('Authorization', sa_token)
-    .set('X-Subject', subject)
-    .set('X-Subject-Group', `developer`)
-    .set('X-Namespace', `${namespace}`)
-    .expect(200, done);
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', sa_token)
+        .set('X-Subject', subject)
+        .set('X-Subject-Group', `developer`)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(200, done);
   });
   it('responds to / for unauthorized user', function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','application/json')
-    .set('Authorization', sa_token)
-    .set('X-Subject', `foo@bar.com`)
-    .set('X-Subject-Group', ``)
-    .set('X-Namespace', `${namespace}`)
-    .expect(401, done);
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', sa_token)
+        .set('X-Subject', `foo@bar.com`)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(401, done);
   });
   it('responds to / for user header variable not set', function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','application/json')
-    .set('Authorization', sa_token)
-    .set('X-Subject', ``)
-    .set('X-Subject-Group', ``)
-    .set('X-Namespace', `${namespace}`)
-    .expect(400, done);
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', sa_token)
+        .set('X-Subject', ``)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(400, done);
   });
   it('responds to / for unauthorized namespace', function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','application/json')
-    .set('Authorization', sa_token)
-    .set('X-Subject', subject)
-    .set('X-Subject-Group', ``)
-    .set('X-Namespace', `foobar_error_namespace`)
-    .expect(403, done);
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', sa_token)
+        .set('X-Subject', subject)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `foobar_error_namespace`)
+        .expect(403, done);
   });
   it(`responds to / for user "${subject}" with bad/unauthorized Service Account token.`, function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','application/json')
-    .set('Authorization', `Bearer ThiswillfailServiceAccountToken`)
-    .set('X-Subject', subject)
-    .set('X-Subject-Group', ``)
-    .set('X-Namespace', `${namespace}`)
-    .expect(401, done);
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', `Bearer ThiswillfailServiceAccountToken`)
+        .set('X-Subject', subject)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(401, done);
+  });
+  it(`responds to / for wrong host`, function (done) {
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('Authorization', sa_token)
+        .set('X-Subject', subject)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', "badhost.address123444322.commer")
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(404, done);
   });
   it('responds to / for bad "Accept" header variable not set to "json"', function (done) {
-  request(server)
-    .get('/')
-    .set('Accept','foo/bar')
-    .set('Authorization', sa_token)
-    .set('X-Subject', subject)
-    .set('X-Subject-Group', ``)
-    .set('X-Namespace', `${namespace}`)
-    .expect(400, done);
+    request(server)
+        .get('/')
+        .set('Accept','foo/bar')
+        .set('Authorization', sa_token)
+        .set('X-Subject', subject)
+        .set('X-Subject-Group', ``)
+        .set('X-Oauth-Host', host)
+        .set('X-Oauth-Port', port)
+        .set('X-Namespace', `${namespace}`)
+        .expect(400, done);
+  });
+  it(`responds to / for none of required headers set.`, function (done) {
+    request(server)
+        .get('/')
+        .set('Accept','application/json')
+        .set('X-Subject-Group', `not required`)
+        .expect(400, done);
   });
   it('404 everything else', function (done) {
     request(server)
