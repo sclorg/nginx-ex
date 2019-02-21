92267a61adcf8e9b7daf3c02e0e9ee08bed16bed
devcomb <james@devcomb.com>
2019-02-21 19:51:14 +0000

    fixup

--- .s2i/dos2unix.js
+++ .s2i/dos2unix.js
@@ -7,15 +7,12 @@ const dos2unix = require('ssp-dos2unix-js').dos2unix
  fs.readdir(dir, function(err, items) { 
     for (var i=0; i<items.length; i++) {
         var path = dir + items[i];
-        fs.chmod(path, 0o660 , (err) => {
-            if (err) throw err;
-        });
-        let converted = dos2unix(path, {feedback: true, writable: true});
-        fs.chmod(path, 0o770 , (err) => {
-            if (err) throw err;
-        });
-        // console.log(converted) // Returned text as string without any carriage returns (\r).
-        // If you use writable as true, returns 0 instead of text.
-        console.log(items[i] + " converted.");
+        let converted = dos2unix(path, {feedback: true, writable: false});
+        fs.writeFile(path, converted, {mode:0o774}, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log(items[i] + " converted.");
+        }); 
     }
 });
